10caf95f5fac754173f8efa611c73396d50bfa37
Denise-juli <112222707+Denise-juli@users.noreply.github.com>
2023-02-26 15:34:42 -0300

    S

--- doc/bd_oasis.sql
+++ doc/bd_oasis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-01-2023 a las 14:51:20
+-- Tiempo de generación: 25-02-2023 a las 16:58:34
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,6 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `usuario_id` int(11) NOT NULL,
+  `servicio_id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `comentario`
 --
 
@@ -32,32 +44,21 @@ CREATE TABLE `comentario` (
   `contenido` text NOT NULL,
   `calificacion` int(11) NOT NULL,
   `fecha_comentario` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `fk_servicio` int(11) NOT NULL
+  `servicio_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `comentario`
 --
 
-INSERT INTO `comentario` (`id_comentario`, `contenido`, `calificacion`, `fecha_comentario`, `fk_servicio`) VALUES
-(1, 'este tratamiento muuuy bueno', 9, '2022-12-23 00:20:10', 1),
-(8, 'esta re lindo el lugar!', 7, '2022-12-24 00:20:16', 2),
-(9, 'no me gusto la atencion del personal', 3, '2022-12-25 00:20:21', 11),
-(14, 'me encanto', 8, '2022-12-26 00:22:29', 6),
-(15, 'la calidad no es tan buena', 5, '2022-12-27 00:23:28', 9),
-(16, 'que relajaciooon', 7, '2022-12-28 00:26:04', 10);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `comentario_x_producto`
---
-
-CREATE TABLE `comentario_x_producto` (
-  `id_xcomentario` int(11) NOT NULL,
-  `fk_servicio` int(11) NOT NULL,
-  `fk_comentario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `comentario` (`id_comentario`, `contenido`, `calificacion`, `fecha_comentario`, `servicio_id`, `usuario_id`) VALUES
+(1, 'este tratamiento muuuy bueno', 9, '2022-12-23 00:20:10', 1, 0),
+(8, 'esta re lindo el lugar!', 7, '2022-12-24 00:20:16', 2, 0),
+(9, 'no me gusto la atencion del personal', 3, '2022-12-25 00:20:21', 11, 0),
+(14, 'me encanto', 8, '2022-12-26 00:22:29', 6, 0),
+(15, 'la calidad no es tan buena', 5, '2022-12-27 00:23:28', 9, 0),
+(16, 'que relajaciooon', 7, '2022-12-28 00:26:04', 10, 0);
 
 -- --------------------------------------------------------
 
@@ -69,18 +70,30 @@ CREATE TABLE `compra` (
   `compra_id` int(11) NOT NULL,
   `fecha_compra` datetime NOT NULL,
   `total` float NOT NULL,
-  `fk_servicio_id` int(11) NOT NULL
+  `servicio_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `compra`
 --
 
-INSERT INTO `compra` (`compra_id`, `fecha_compra`, `total`, `fk_servicio_id`) VALUES
+INSERT INTO `compra` (`compra_id`, `fecha_compra`, `total`, `servicio_id`) VALUES
 (1, '2022-11-27 07:02:41', 5200, 1),
 (2, '2022-11-27 07:02:41', 5200, 1),
 (3, '2022-11-27 07:03:50', 6000, 3),
 (4, '2022-11-27 07:04:33', 8050, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puntos`
+--
+
+CREATE TABLE `puntos` (
+  `usuario_id` int(11) NOT NULL,
+  `servicio_id` int(11) NOT NULL,
+  `puntaje` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -171,7 +184,6 @@ CREATE TABLE `usuario` (
   `password` varchar(10) NOT NULL,
   `telefono` varchar(11) DEFAULT NULL,
   `fecha_nac` datetime DEFAULT NULL,
-  `puntos` int(11) DEFAULT NULL,
   `fk_tipo_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -179,18 +191,18 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`usuario_id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `fecha_nac`, `puntos`, `fk_tipo_id`) VALUES
-(1, 'Vanesa', 'Martinez', 'vanmar@gmail.com', '1234', '1254-4789', '1980-11-01 03:00:28', 55, 1),
-(3, 'Matias', 'Artiga', 'matiart@gmail.com', '1234', '1254-4756', '1956-10-05 00:00:00', 120, 1),
-(5, 'Camila', 'Estuardo', 'camiestuardo@gmail.com', '1234', '1254-1259', '1999-08-17 00:00:00', 80, 2),
-(8, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL),
-(9, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL),
-(10, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL),
-(11, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL),
-(12, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL),
-(13, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL),
-(14, '', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL),
-(15, 'vanea', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL, NULL);
+INSERT INTO `usuario` (`usuario_id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `fecha_nac`, `fk_tipo_id`) VALUES
+(1, 'Vanesa', 'Martinez', 'vanmar@gmail.com', '1234', '1254-4789', '1980-11-01 03:00:28', 1),
+(3, 'Matias', 'Artiga', 'matiart@gmail.com', '1234', '1254-4756', '1956-10-05 00:00:00', 1),
+(5, 'Camila', 'Estuardo', 'camiestuardo@gmail.com', '1234', '1254-1259', '1999-08-17 00:00:00', 2),
+(8, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL),
+(9, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL),
+(10, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL),
+(11, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL),
+(12, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL),
+(13, 'valeria', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL),
+(14, '', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL),
+(15, 'vanea', '', 'vanmar@gmail.com', '1234', NULL, NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -201,22 +213,14 @@ INSERT INTO `usuario` (`usuario_id`, `nombre`, `apellido`, `email`, `password`, 
 --
 ALTER TABLE `comentario`
   ADD PRIMARY KEY (`id_comentario`),
-  ADD KEY `servicio_fk` (`fk_servicio`);
-
---
--- Indices de la tabla `comentario_x_producto`
---
-ALTER TABLE `comentario_x_producto`
-  ADD PRIMARY KEY (`id_xcomentario`),
-  ADD KEY `fk_servicio` (`fk_servicio`),
-  ADD KEY `fk_comentario` (`fk_comentario`);
+  ADD KEY `servicio_fk` (`servicio_id`);
 
 --
 -- Indices de la tabla `compra`
 --
 ALTER TABLE `compra`
   ADD PRIMARY KEY (`compra_id`),
-  ADD KEY `fk_servicio_id` (`fk_servicio_id`);
+  ADD KEY `fk_servicio_id` (`servicio_id`);
 
 --
 -- Indices de la tabla `servicio`
@@ -256,12 +260,6 @@ ALTER TABLE `comentario`
   MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT de la tabla `comentario_x_producto`
---
-ALTER TABLE `comentario_x_producto`
-  MODIFY `id_xcomentario` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
@@ -299,20 +297,13 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  ADD CONSTRAINT `servicio_fk` FOREIGN KEY (`fk_servicio`) REFERENCES `servicio` (`servicio_id`);
-
---
--- Filtros para la tabla `comentario_x_producto`
---
-ALTER TABLE `comentario_x_producto`
-  ADD CONSTRAINT `fk_comentario` FOREIGN KEY (`fk_comentario`) REFERENCES `comentario` (`id_comentario`),
-  ADD CONSTRAINT `fk_servicio` FOREIGN KEY (`fk_servicio`) REFERENCES `servicio` (`servicio_id`);
+  ADD CONSTRAINT `servicio_fk` FOREIGN KEY (`servicio_id`) REFERENCES `servicio` (`servicio_id`);
 
 --
 -- Filtros para la tabla `compra`
 --
 ALTER TABLE `compra`
-  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`fk_servicio_id`) REFERENCES `servicio` (`servicio_id`);
+  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`servicio_id`) REFERENCES `servicio` (`servicio_id`);
 
 --
 -- Filtros para la tabla `turno`
